a0f8d18e15b75788cf0cb7c051ed8519e8040e23
unknown <juans.mejia@innteligen.com>
2026-08-24 16:46:18 -0500

    change comment

--- src/config/features.js
+++ src/config/features.js
@@ -1,5 +1,4 @@
 /**
- * Turns the Projects section (and its nav link) off until there are real
- * case studies to show.
+ * Enable or disable this option if you have projects to showcase in your portfolio.
  */
 export const SHOW_PROJECTS = false;
